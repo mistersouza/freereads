3f98ac3edc3a93e3afe971c2347f02cc135f65b4
Thiago Souza <mistersouza@icloud.com>
2025-03-04 21:47:02 +0100

    feat(errors): implement comprehensive error handling middleware system #13

--- WD-BE/src/middlewares/auth-middleware.js
+++ WD-BE/src/middlewares/auth-middleware.js
@@ -1,49 +1,42 @@
 import { verifyToken } from '../utils/jwt-handler.js';
 import User from '../models/User.js';
 import ApiError from '../errors/api-error.js';
-import { ENV } from '../config/env.js';
 import { AUTH_ERRORS } from '../constants/error-messages.js';
 
 const authorize = (roles = ['boss', 'overlord']) => async (request, response, next) => {
-    let error = null;
-    const { id } = request.params;
-
     if (!request.headers.authorization?.startsWith('Bearer ')) {
-        error = new ApiError(AUTH_ERRORS.TOKEN_MISSING, 401);
+        return next(new ApiError(AUTH_ERRORS.TOKEN_MISSING, 401))
     }
 
-    if (!error) {
-        const token = request.headers.authorization.split(' ')[1];
-        try {
-            const decoded = verifyToken(token);
+    const token = request.headers.authorization.split(' ')[1];
 
-            if (id) {
-                const requestedUser = await User.findById(id);
+    try {
+        const decoded = verifyToken(token);
 
-                if (!requestedUser) {
-                    error = new ApiError(AUTH_ERRORS.NOT_FOUND, 404);
-                }
+        const { id } = request.params;
 
-                if (!roles.includes(decoded.role) && decoded.id !== id) {
-                    error = new ApiError(AUTH_ERRORS.ACCESS_DENIED, 403);
-                }
+        if (id) {
+            const requestedUser = await User.findById(id);
+
+            if (!requestedUser) {
+                return next(new ApiError(AUTH_ERRORS.NOT_FOUND, 404));
             }
 
-            if (!id) {
-                if (!roles.includes(decoded.role)) {
-                    error = new ApiError(AUTH_ERRORS.ACCESS_DENIED, 403);
-                }
+            if (!roles.includes(decoded.role) && decoded.id !== id) {
+                return next(new ApiError(AUTH_ERRORS.ACCESS_DENIED, 403));
             }
-        } catch (jwtError) {
-            error = new ApiError(
-                jwtError.expiredAt 
-                    ? AUTH_ERRORS.EXPIRED_TOKEN 
-                    : AUTH_ERRORS.INVALID_TOKEN, 
-                401
-            );
         }
+
+        if (!id) {
+            if (!roles.includes(decoded.role)) {
+                return next(new ApiError(AUTH_ERRORS.ACCESS_DENIED, 403));
+            }
+        }
+        next();
+    } catch (jwtError) {
+        next(jwtError);
     }
-    next(error);
 };
 
 export default authorize;
+
