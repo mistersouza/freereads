aac3860d14d3408ed6da29c7ea7303a9048e2175
Thiago Souza <mistersouza@icloud.com>
2025-03-05 20:28:37 +0100

    chore(imports): update user import to new file naming

--- WD-BE/src/middlewares/auth-middleware.js
+++ WD-BE/src/middlewares/auth-middleware.js
@@ -1,5 +1,5 @@
 import { verifyToken } from '../utils/jwt-handler.js';
-import User from '../models/User.js';
+import User from '../models/user-model.js';
 import ApiError from '../errors/api-error.js';
 import {
     AUTH_ERROR_MESSAGES,
