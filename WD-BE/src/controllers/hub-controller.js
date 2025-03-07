f52c9c68b25d995f72b2b61c37fb882fc314d3d1
Thiago Souza <mistersouza@icloud.com>
2025-03-07 22:37:57 +0100

    fix(hubs): sort function name mismatches between controller factory and hub controller

--- WD-BE/src/controllers/hub-controller.js
+++ WD-BE/src/controllers/hub-controller.js
@@ -272,8 +272,8 @@ import controllerFactory from '../utils/controller-factory.js';
 const { 
     getAll: getAllHubs,
     getOne: getOneHub,
-    create: createHub,
-    update: updateHub,
+    createOne: createHub,
+    updateOne: updateHub,
     deleteOne: deleteHub 
 } = controllerFactory(Hub, 'hubs');
 
