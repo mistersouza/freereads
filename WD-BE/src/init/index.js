0f93a6afacfa74452f553c7f17fc35ed096c12da
Thiago Souza <mistersouza@icloud.com>
2025-03-21 19:42:47 +0100

    refactor(sctrucre): consolidate application initialization into services.js

--- WD-BE/src/init/index.js
+++ WD-BE/src/init/index.js
@@ -1,28 +1,18 @@
-import { connectDB } from './db.js';
-import { connectRedis } from './redis.js';
-import { setRemoteStore } from '../init/redis.js';
+import { initializeServices } from './services.js';
 import { log } from '../errors/index.js';
-import { setRateLimiter, setSpeedLimiter } from '../middlewares/rate-limiter-middleware.js';
-
 
 /**
- * Initializes the application by connecting to the database and Redis,
- * setting up the remote store, and configuring rate and speed limiters.
+ * Initializes core application services for request control and book finding.
  * 
- * @returns {Array} An array of configured rate and speed limiters
- * @throws {Error} Logs any initialization errors and exits the process
+ * @returns {Object} An object containing initialized services from request control and book finder
  */
 const kickstart = async () => {
     try {
-        await connectDB();
-        await connectRedis();
-        const { store } = setRemoteStore();
-
-        return [ setSpeedLimiter(store), setRateLimiter(store) ];
-    } catch (error) {
+        return await initializeServices();
+    }
+    catch (error) {
         log.error(error);
-        // TODO: Streamline error handling into a unified system across the app
-        process.exit(1);
+        process.exit(1); 
     }
 };
 
