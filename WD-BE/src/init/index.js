da2f0baa223d79f9d350ca54e290db1eb11a3c07
Thiago Souza <mistersouza@icloud.com>
2025-04-21 17:13:49 +0200

    enhance(bootstrap): improve service initialization logging and error handling

--- WD-BE/src/init/index.js
+++ WD-BE/src/init/index.js
@@ -3,17 +3,21 @@ import { log } from '../services/error/index.js';
 
 /**
  * Initializes core application services for request control and book finding.
- * 
+ *
  * @returns {Object} An object containing initialized services from request control and book finder
  */
+
+// eslint-disable-next-line consistent-return
 const bootstrapServices = async () => {
-    try {
-        return await initializeServices();
-    }
-    catch (error) {
-        log.error(error);
-        process.exit(1); 
-    }
+  try {
+    const services = await initializeServices();
+    log.info('Services are up and running.');
+    return services;
+  } catch (error) {
+    log.warn('Something went wrong while spinning up services.');
+    log.error(error);
+    process.exit(1);
+  }
 };
 
 export { bootstrapServices };
